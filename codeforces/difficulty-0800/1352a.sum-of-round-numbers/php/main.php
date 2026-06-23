65d8b6eb835b25b942fd06ef4cd31de6117ce5a9
Shihab Mahamud <shihab4t@gmail.com>
2026-06-24 01:21:00 +0600

    wip

--- codeforces/difficulty-0800/1352a.sum-of-round-numbers/php/main.php
+++ codeforces/difficulty-0800/1352a.sum-of-round-numbers/php/main.php
@@ -8,5 +8,25 @@ while ($t--) {
 
 function testCase()
 {
-    echo "Hello World\n";
+    $n = intval(trim(fgets(STDIN)));
+    $x = 10;
+
+    $a = [];
+
+    while ($n) {
+        $r = $n % $x;
+        if ($r) {
+            array_push($a, $r);
+        }
+        $n -= $r;
+        $x *= 10;
+    }
+
+    $l = count($a);
+
+    echo "{$l}\n";
+    foreach ($a as $i) {
+        echo "{$i} ";
+    }
+    echo "\n";
 }
--- codeforces/templates/main.php
+++ codeforces/templates/main.php
@@ -8,25 +8,5 @@ while ($t--) {
 
 function testCase()
 {
-    $n = intval(trim(fgets(STDIN)));
-    $x = 10;
-
-    $a = [];
-
-    while ($n) {
-        $r = $n % $x;
-        if ($r) {
-            array_push($a, $r);
-        }
-        $n -= $r;
-        $x *= 10;
-    }
-
-    $l = count($a);
-
-    echo "{$l}\n";
-    foreach ($a as $i) {
-        echo "{$i} ";
-    }
-    echo "\n";
+    echo "Hello World\n";
 }
