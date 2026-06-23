c659ca0d2d15da35381ecc7c94d1ad460c5f2b17
Shihab Mahamud <shihab4t@gmail.com>
2026-06-23 22:34:08 +0600

    wip

---
+++ codeforces/difficulty-0800/1352a.sum-of-round-numbers/js/main.js
@@ -0,0 +1,38 @@
+"use strict";
+
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+
+let input = "";
+let idx = 0;
+
+process.stdin.on("data", chunk => input += chunk);
+process.stdin.on("end", () => { input = input.trim().split("\n");main()});
+
+function readline() {
+    return input[idx++];
+}
+
+function main() {
+    let t = Number(readline());
+    while (t--) {
+        testCase();
+    }
+}
+
+function testCase() {
+    let n = Number(readline());
+    let x = 10;
+    const a = [];
+    while(n) {
+        const r = n % x;
+        if (r) {
+            a.push(r);
+        }
+        n -= r;
+        x *= 10;
+    }
+    console.log(a.length)
+    console.log(a.reduce((r, c) => !r ? c : `${r} ${c}`, ""))
+}
+
