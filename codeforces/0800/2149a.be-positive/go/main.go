71f3d8fc683e06e160b1aa94b578f8c129acda44
Shihab Mahamud <shihab4t@gmail.com>
2026-06-24 16:23:53 +0600

    go

--- codeforces/0800/2149a.be-positive/go/main.go
+++ codeforces/0800/2149a.be-positive/go/main.go
@@ -12,5 +12,21 @@ func main() {
 }
 
 func testCase() {
-	fmt.Println("Hello World")
+	var n int
+	fmt.Scan(&n)
+	zero_count := 0
+	negative_count := 0
+	for i := 0; i < n; i++ {
+		var t int
+		fmt.Scan(&t)
+		switch t {
+		case 0:
+			zero_count++
+		case -1:
+			negative_count++
+		}
+	}
+	r := zero_count
+	r += ((negative_count % 2) * 2)
+	fmt.Println(r)
 }
